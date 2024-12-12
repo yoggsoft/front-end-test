cae39de6cfe5e42ae6b3a666ef340e89e36ffcc1
yoggsoft <yoggsoft@gmail.com>
2024-12-13 00:14:15 +0100

    handling background html - not gonna do responsive stuff

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -8,8 +8,8 @@ export default function Container({
   children
 }: Readonly<ContainerProps>) {
   return (
-    <main className="bg-grey w-full h-full py-6 px-6  pt-[80px]">
-      <div className="bg-white mx-auto px-4 py-6 sm:px-6 lg:px-8 rounded-xl border border-grey-extra-light shadow">
+    <main className='block w-full h-height bg-grey py-6 px-6  pt-[80px] m-0'>
+      <div className='bg-white mx-auto px-4 py-6 sm:px-6 lg:px-8 rounded-xl border border-grey-extra-light shadow'>
         {children}
       </div>
     </main>
